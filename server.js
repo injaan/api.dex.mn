b5ddec9e684b6b23b317dedb16826500d02602c9
Tolui <tobi418@gmail.com>
2021-05-10 23:38:45 +0800

    cors added

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
+var cors = require('cors')
 const port = process.env.PORT || '3001';
 
 //routes
 const indexRouter = require('./routes/indexRouter');
 
+var corsOptions = {
+    origin: 'https://dex.mn',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 var app = express();
+app.use(cors(corsOptions))
 app.use(helmet());
 app.use(express.json({limit:'3mb'}));
 app.use(express.urlencoded({ extended: false }));
